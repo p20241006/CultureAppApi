fef492d8cbf5f4d28c3459c28d8de2cfb0fea8b6
Wilfredo Futuri Illa <wfuturi@indecopi.gob.pe>
2024-08-23 14:17:34 -0500

    feat: first commit app v1

---
+++ src/main/java/cultureapp/com/pe/history/BookTransactionHistory.java
@@ -0,0 +1,31 @@
+package cultureapp.com.pe.history;
+
+import cultureapp.com.pe.event.Book;
+import cultureapp.com.pe.common.BaseEntity;
+import cultureapp.com.pe.user.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class BookTransactionHistory extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+    private boolean returned;
+    private boolean returnApproved;
+}
---
+++ src/main/java/cultureapp/com/pe/user/UserRepository.java
@@ -0,0 +1,9 @@
+package cultureapp.com.pe.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String username);
+}
